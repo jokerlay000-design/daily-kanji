a44e00f3c45ae10f4d7fb684fb23b60542aa304c
jokerlay000-design <jokerlay000@gmail.com>
2026-06-07 09:23:27 +0900

    Add user management with nickname

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2026_05_26_120308) do
+ActiveRecord::Schema[7.1].define(version: 2026_06_06_232800) do
   create_table "kanjis", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.string "character"
     t.string "onyomi"
@@ -31,6 +31,7 @@ ActiveRecord::Schema[7.1].define(version: 2026_05_26_120308) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "nickname"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
